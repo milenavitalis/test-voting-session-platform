ace9721589d9a4df977cdbb2ff685eb4b5e8acde
milenavitalis <milenagoetzevitalis@outlook.com>
2025-07-19 11:25:58 -0300

    fix: validation of CPF already registered in the login route

--- backend/app/logic/user.py
+++ backend/app/logic/user.py
@@ -1,17 +1,23 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.future import select
 from app.models import User
 from app.schemas.user import UserCreate
+from app.logic.utils import clean_cpf 
 import bcrypt
 
-def create_user(user_data: UserCreate, db: Session):
+async def create_user(user_data: UserCreate, db):
+    cpf = clean_cpf(user_data.cpf)
     hashed_password = bcrypt.hashpw(user_data.password.encode('utf-8'), bcrypt.gensalt())
-
+    print("Criando usuário", hashed_password)
+    result = await db.execute(select(User).where(User.cpf == cpf))
+    existing_user = result.scalars().first()
+    if existing_user:
+        raise ValueError("CPF já cadastrado")
     user = User(
         name=user_data.name,
-        cpf=user_data.cpf,
+        cpf=cpf,
         password_hash=hashed_password.decode('utf-8')
     )
     db.add(user)
-    db.commit()
-    db.refresh(user)
+    await db.commit()
+    await db.refresh(user)
     return user
